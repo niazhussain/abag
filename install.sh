2a30118516b07a3d7c2ea7ac94b5c3cd88b24b85
Hussain <niazhussain1990@gmail.com>
2020-06-30 19:29:52 +0200

    system reboot command is removed..

--- install.sh
+++ install.sh
@@ -16,8 +16,7 @@ sudo apt-get install -y docker-ce docker-ce-cli containerd.io
 sudo groupadd docker
 
 sudo usermod -aG docker $USER
-sudo chmod 666 /var/run/docker.sock
 
 sudo curl -L "https://github.com/docker/compose/releases/download/$DOCKER_COMPOSE_VERSION/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
-sudo reboot
+sudo newgrp docker
